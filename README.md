2efb71435245bc72312f88885e44a5cb4f957e36
Aayush Kumar <>
2021-05-18 18:02:14 +0000

    updated readme, added email api

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Made with:
 - Next.js
 - TailwindCSS
+
+# Screenshots
+![full page](https://i.ibb.co/7VT4BN8/image.png)
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,40 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const mailjet = require("node-mailjet").connect(
+  process.env['MAILJET_1'],
+  process.env['MAILJET_2']
+);
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+module.exports = async (req, res) => {
+  // this function will be launched from Hasura event
+  const { body } = req;
+  try {
+    const recipient = body.event.data.new;
+    const result = await mailjet.post("send", { version: "v3.1" }).request({
+      Messages: [
+        {
+          From: {
+            Email: "lwar780@gmail.com",
+            Name: "Skip Schedules",
+          },
+          To: [
+            {
+              Email: recipient.email,
+            },
+          ],
+          Subject: "Greetings from Skip Schedules!",
+          TextPart: recipient.name +
+            "You have a meeting scheduled in the next 30 mins.",
+          HTMLPart:
+            "<p>Here is a link to your meeting! {link}</p>",
+        },
+      ],
+    });
+
+    res.status(200).json({
+      method: req.method,
+      msg: "Received the event and sent the email to the user!",
+      result: result.body,
+    });
+  } catch (err) {
+    res.send(err); // send the thrown error
+  }
+};
